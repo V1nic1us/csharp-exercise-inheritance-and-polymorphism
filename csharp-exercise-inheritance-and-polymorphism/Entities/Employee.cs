3b18b83570d1aec12f1f303d50228a12733214a2
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-04 18:22:16 -0400

    feat: add employee class

---
+++ csharp-exercise-inheritance-and-polymorphism/Entities/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercise_inheritance_and_polymorphism.Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee() { }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+        public virtual double payment()
+        {
+            return Hours * ValuePerHour;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Payments");
+            sb.Append(Name);
+            sb.Append(" - R$");
+            sb.AppendLine(payment().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
