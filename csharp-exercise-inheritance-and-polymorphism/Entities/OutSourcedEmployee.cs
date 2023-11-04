37be0054d084d93303d250aef1455289034e2daf
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-04 18:22:51 -0400

    feat: add out sourced employee class

---
+++ csharp-exercise-inheritance-and-polymorphism/Entities/OutSourcedEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercise_inheritance_and_polymorphism.Entities
+{
+    internal class OutSourcedEmployee: Employee
+    {
+        public double AdditionalChange { get; set; }
+        public OutSourcedEmployee() { }
+
+        public OutSourcedEmployee(string name, int hours, double valuePerHour, double additionalChange):base(name, hours, valuePerHour)
+        {
+            AdditionalChange = additionalChange;
+        }
+
+        public override double payment()
+        {
+            return base.payment() + AdditionalChange * 1.10;
+        }
+    }
+}
